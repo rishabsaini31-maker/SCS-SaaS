7b9fafda0225d4e87540553c7f7fa95a2cb47e3f
Rishab Saini <rishabsainiupw165@gmail.com>
2026-07-18 01:39:50 +0530

    Fix scs-super-admin empty data: add localhost:3001 to CORS allowlist

--- server/src/common/config/index.ts
+++ server/src/common/config/index.ts
@@ -151,6 +151,7 @@ export const config = {
     ? env.CORS_ORIGINS.split(",")
     : [
         "http://localhost:3000",
+        "http://localhost:3001",
         "http://localhost:3002",
         "http://localhost:3003",
       ],
